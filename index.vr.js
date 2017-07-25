deca2553882806c1a631279f13d2f88b01aa9b04
Daniel Park <dpeace926@gmail.com>
2017-07-25 01:13:25 -0400

    added new wtc pano img

--- index.vr.js
+++ index.vr.js
@@ -11,10 +11,10 @@ export default class WelcomeToVR extends React.Component {
   render() {
     return (
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
+        <Pano source={asset('wtc_path_station.jpg')}/>
         <Text
           style={{
-            backgroundColor: '#777879',
+            backgroundColor: '#ff6f69',
             fontSize: 0.8,
             fontWeight: '400',
             layoutOrigin: [0.5, 0.5],
@@ -22,9 +22,9 @@ export default class WelcomeToVR extends React.Component {
             paddingRight: 0.2,
             textAlign: 'center',
             textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
+            transform: [{translate: [0, 0, -9]}],
           }}>
-          hello
+          Hi, Floored!
         </Text>
       </View>
     );
